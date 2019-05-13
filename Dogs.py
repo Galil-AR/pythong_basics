b4fafe96e7727636b3b6bb1fe2522d8a8b5216b2
Emerson Castillo <luffygamingmc@gmail.com>
2019-05-13 11:55:14 -0400

    Added spaces and made syntax more readable

--- Dogs.py
+++ Dogs.py
@@ -2,6 +2,11 @@ dogs = ['border collie', 'pitbull', 'husky', 'golden retriever', 'german shepard
 
 dog = dogs[0]
 print(dog.title())
+print()
+
 for dog in dogs:
     print("The " + dog.title())
 
+dog = dogs[1]
+print()
+print(dog.title())
